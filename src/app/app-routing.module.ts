794c85c05c27334f55d05d9caf8c6ab96eed9145
colbyhill21 <acehill21@ymail.com>
2020-07-12 21:00:45 -0600

    Added basic app routing and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppDownloadComponent } from './app-download/app-download.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'appdownload', component: AppDownloadComponent },
+  { path: '*', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppDownloadComponent } from './app-download/app-download.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppDownloadComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
